457f3021ca2ff6253dac647ba31b8ab6f14d51f7
treesukon <treesukon.bua@gmail.com>
2022-07-14 13:28:47 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-          <h1>Hello world</h1>
+          <h1>Treesukon Buaklee</h1>
           <button onClick >OK</button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
